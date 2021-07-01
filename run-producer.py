76638b7d4fda54aaf81668794df2d3dcc06cee4f
Michael Berg-Mohnicke <michael.berg-mohnicke@zalf.de>
2021-07-01 15:52:20 +0200

    dev dump

--- run-producer.py
+++ run-producer.py
@@ -40,16 +40,16 @@ PATHS = {
     # adjust the local path to your environment
     "mbm-local-remote": {
         "include-file-base-path": "/home/berg/GitHub/monica-parameters/", # path to monica-parameters
-        "path-to-climate-dir": "/run/user/1000/gvfs/sftp:host=login01.cluster.zalf.de,user=rpm/beegfs/common/data/climate/dwd/cmip_cordex_reklies/", # mounted path to archive or hard drive with climate data 
-        "monica-path-to-climate-dir": "/monica_data/climate-data/dwd/cmip_cordex_reklies/csv/", # mounted path to archive accessable by monica executable
+        "path-to-climate-dir": "/run/user/1000/gvfs/sftp:host=login01.cluster.zalf.de,user=rpm/beegfs/common/data/climate/dwd_core_ensemble/", # mounted path to archive or hard drive with climate data 
+        "monica-path-to-climate-dir": "/monica_data/climate-data/dwd_core_ensemble/csvs_dwd_core_ensemble/", # mounted path to archive accessable by monica executable
         "path-to-data-dir": "./monica-data/data/", # mounted path to archive or hard drive with data 
         "path-to-projects-dir": "./monica-data/data/projects/",
         "path-debug-write-folder": "./debug-out/",
     },
     "remoteProducer-remoteMonica": {
         "include-file-base-path": "/project/monica-parameters/", # path to monica-parameters
-        "path-to-climate-dir": "/data/dwd/cmip_cordex_reklies/", # mounted path to archive or hard drive with climate data 
-        "monica-path-to-climate-dir": "/monica_data/climate-data/dwd/cmip_cordex_reklies/csv/", # mounted path to archive accessable by monica executable
+        "path-to-climate-dir": "/data/dwd_core_ensemble/", # mounted path to archive or hard drive with climate data 
+        "monica-path-to-climate-dir": "/monica_data/climate-data/dwd_core_ensemble/csvs_dwd_core_ensemble/", # mounted path to archive accessable by monica executable
         "path-to-data-dir": "./monica-data/data/", # mounted path to archive or hard drive with data 
         "path-to-projects-dir": "./monica-data/data/projects/", # mounted path to archive or hard drive with project data 
         "path-debug-write-folder": "/out/debug-out/",
