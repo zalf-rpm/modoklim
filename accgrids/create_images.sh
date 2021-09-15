4ef376091fc866eee977d9ae74bd02fd8faf2567
sanny <38938782+sannyschulz@users.noreply.github.com>
2021-09-15 17:44:03 +0200

    image gen

---
+++ accgrids/create_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=80
+#SBATCH --partition=compute
+#SBATCH --job-name=go_ascii
+#SBATCH --time=10:00:00
+
+IMAGE_DIR_PYTHON=~/singularity/python
+SINGULARITY_PYTHON_IMAGE=python3.7_2.0.sif
+IMAGE_PYTHON_PATH=${IMAGE_DIR_PYTHON}/${SINGULARITY_PYTHON_IMAGE}
+mkdir -p $IMAGE_DIR_PYTHON
+if [ ! -e ${IMAGE_PYTHON_PATH} ] ; then
+echo "File '${IMAGE_PYTHON_PATH}' not found"
+cd $IMAGE_DIR_PYTHON
+singularity pull docker://zalfrpm/python3.7:2.0
+cd ~
+fi
+
+FOLDER=../agg_grids/
+mkdir -p $FOLDER/img
+IMG=~/singularity/python/python3.7_2.0.sif
+singularity run -B $FOLDER:/source,$FOLDER/img:/out $IMG python create_image_from_ascii.py path=cluster
