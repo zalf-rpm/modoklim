5c76bdf08f68fd0e6e406c4fed6d0ef5443a3aee
Michael Berg-Mohnicke <michael.berg-mohnicke@zalf.de>
2021-10-07 10:31:38 +0200

    update script

--- update_-999_to_-9999.py
+++ update_-999_to_-9999.py
@@ -17,21 +17,48 @@
 
 import in_place
 import os
+import sys
 
-def update_files(files):
+def in_place_update_files(dir, files):
+    files.sort()
     for file in files:
+        #print("file:", file)
+        #continue
         if file.endswith(".asc"):
-            with in_place.InPlace(os.path.join(root, file)) as fp:
+            with in_place.InPlace(os.path.join(dir, file)) as fp:
                 for i, line in enumerate(fp):
                     if i == 5:
                         line = line.replace("-999", "-9999")
                     fp.write(line) 
 
-if len(sys.argv) > 1:
-    for root, dirs, files in os.walk(argv[1]):
-        if len(dirs) > 0:
-            for dir in dirs:
-                for root, dirs, files in os.walk(os.path.join(root, dir)):
-                    update_files(files)
-        if len(files) > 0:
-            update_files(files)
+def copy_update_files(from_dir, files, to_dir):
+    #print("copy files from:", from_dir, "to dir:", to_dir)
+    files.sort()
+    for file in files:
+        #print("file:", os.path.join(from_dir, file), " to:", os.path.join(to_dir, file))
+        #continue
+        if file.endswith(".asc"):
+            with open(os.path.join(from_dir, file), mode="r") as fp:
+                with open(os.path.join(to_dir, file), mode="w") as tp:
+                    for i, line in enumerate(fp):
+                        if i == 5:
+                            line = line.replace("-999", "-9999")
+                        tp.write(line) 
+
+if len(sys.argv) > 2:
+    from_path = sys.argv[1]
+    to_path = sys.argv[2]
+    from_setup = int(sys.argv[3]) if len(sys.argv) > 3 else 1
+    to_setup = int(sys.argv[4]) if len(sys.argv) > 4 else 136
+    for root1, dirs1, files1 in os.walk(from_path):
+        if len(dirs1) > 0:
+            dirs1.sort()
+            for dir1 in dirs1:
+                if from_setup <= int(dir1) and int(dir1) <= to_setup:
+                    os.makedirs(os.path.join(to_path, dir1), exist_ok=True)
+                    for root2, dirs2, files2 in os.walk(os.path.join(root1, dir1)):
+                        #in_place_update_files(root, files)
+                        copy_update_files(root2, files2, os.path.join(to_path, dir1))
+        if len(files1) > 0:
+            #in_place_update_files(files)
+            copy_update_files(root1, files1, to_path)
