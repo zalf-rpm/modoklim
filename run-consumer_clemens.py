1c016480691a5674957160ab3df98ae7b75b937f
clejae <clemens.jaenicke.1@hu-berlin.de>
2021-11-23 16:23:54 +0100

    small fix

--- run-consumer_clemens.py
+++ run-consumer_clemens.py
@@ -125,6 +125,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
             "s7doy": {"data" : make_dict_nparr(), "cast-to": "int"},
             "hdoy": {"data" : make_dict_nparr(), "cast-to": "int"},
         }
+        output_keys = list(output_grids.keys())
     else:
         output_grids = {
             "Yield": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
