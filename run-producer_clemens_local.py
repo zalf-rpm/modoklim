0e4fe7867a3ac1fcda5ac976ea9191dd42c18278
clejae <clemens.jaenicke.1@hu-berlin.de>
2022-01-05 15:56:08 +0100

    final runs spring barley

--- run-producer_clemens_local.py
+++ run-producer_clemens_local.py
@@ -101,7 +101,7 @@ def run_producer(server = {"server": None, "port": None}, shared_id = None):
         "crop.json": "crop.json",
         "site.json": "site.json",
         "setups-file": "sim_setups_wb_testing.csv",
-        "run-setups": "[302]",
+        "run-setups": "[305]",
         "shared_id": shared_id
     }
     
