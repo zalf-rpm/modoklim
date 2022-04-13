b12169d80f5667fe6553e7f765f577b60818c86e
Ghazaryan <Gohar.Ghazaryan@zalf.de>
2022-04-13 17:06:16 +0200

    Update run-consumer_remote_sensing.py

--- run-consumer_remote_sensing.py
+++ run-consumer_remote_sensing.py
@@ -71,7 +71,7 @@ def create_output(msg):
     cmcs = list(cm_count_to_vals.keys())
     cmcs.sort()
     last_cmc = cmcs[-1]
-    if "year" not in cm_count_to_vals[last_cmc]:
+    if "Year" not in cm_count_to_vals[last_cmc]:
         cm_count_to_vals.pop(last_cmc)
 
     return cm_count_to_vals
