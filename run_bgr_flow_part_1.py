fcbab635e8e7da4e18834a9df9ea398f0e0b1eba
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-06-15 16:21:41 +0200

    preparation for slurm test

---
+++ run_bgr_flow_part_1.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding: UTF-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+# Authors:
+# Michael Berg-Mohnicke <michael.berg-mohnicke@zalf.de>
+#
+# Maintainers:
+# Currently maintained by the authors.
+#
+# This file has been created at the Institute of
+# Landscape Systems Analysis at the ZALF.
+# Copyright (C: Leibniz Centre for Agricultural Landscape Research (ZALF)
+
+import socket
+import subprocess as sp
+import sys
+import uuid
+
+def get_free_port():
+    with socket.socket() as s:
+        s.bind(('',0))
+        return s.getsockname()[1]
+
+def start_channel(path_to_channel, host, chan_port, reader_srt, writer_srt):
+    return sp.Popen([
+        path_to_channel, 
+        "--host={}".format(host),
+        "--name=chan_{}".format(chan_port),
+        "--port={}".format(chan_port),
+        "--reader_srts={}".format(reader_srt),
+        "--writer_srts={}".format(writer_srt),
+    ])
+
+config = {
+    "hpc": False,
+    "shared_in_srt": str(uuid.uuid4()),
+    "shared_channel_port": str(get_free_port()),
+    "use_infiniband": False,
+    "path_to_channel": "/home/berg/GitHub/mas-infrastructure/src/cpp/common/_cmake_linux_release/channel",
+    "path_to_mas": "/home/berg/GitHub/mas-infrastructure",
+    "setups_file": "/home/berg/GitHub/klimertrag/sim_setups_bgr_flow.csv",
+    "coords_file": "/home/berg/Desktop/all_coord_shuffled_anonymous.csv",
+}
+if len(sys.argv) > 1 and __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        k,v = arg.split("=")
+        if k in config:
+            if v.lower() in ["true", "false"]:
+                config[k] = v.lower() == "true"
+            else:
+                config[k] = v
+
+use_infiniband = config["use_infiniband"]
+node_hostname = socket.gethostname()
+node_fqdn = (node_hostname + ".opa" if use_infiniband else ".service") if config["hpc"] else node_hostname
+node_ip = socket.gethostbyname(node_fqdn)
+
+components = []
+channels = []
+rs = [] # reader sturdy ref tokens
+ws = [] # writer sturdy ref tokens
+ps = [] # ports
+
+rs.append(str(uuid.uuid4()))
+ws.append(str(uuid.uuid4()))
+ps.append(get_free_port())
+channels.append(start_channel(config["path_to_channel"], node_ip, ps[-1], rs[-1], ws[-1]))
+
+_ = sp.Popen([
+    "python", 
+    "{}/src/python/fbp/read_csv.py".format(config["path_to_mas"]), 
+    "out_sr=capnp://insecure@{host}:{port}/{srt}".format(host=node_ip, port=ps[-1], srt=ws[-1]),
+    "file="+config["setups_file"],
+    "path_to_capnp_struct=bgr.capnp:Setup",
+    "id_col=runId",
+    "send_ids=1",
+])
+components.append(_)
+
+rs.append(config["shared_in_srt"])
+ws.append(str(uuid.uuid4()))
+ps.append(int(config["shared_channel_port"]))
+channels.append(start_channel(config["path_to_channel"], node_ip, ps[-1], rs[-1], ws[-1]))
+
+channels.append(_)
+_ = sp.Popen([
+    "python", 
+    "{}/src/python/fbp/read_file.py".format(config["path_to_mas"]), 
+    "attr_sr=capnp://insecure@{host}:{port}/{srt}".format(host=node_ip, port=ps[-2], srt=rs[-2]),
+    "to_attr=setup",
+    "out_sr=capnp://insecure@{host}:{port}/{srt}".format(host=node_ip, port=ps[-1], srt=ws[-1]),
+    "skip_lines=1",
+    "file="+config["coords_file"]
+])
+components.append(_)
+
+#-----------------------------------------------------------------------------
+
+for component in components:
+    component.wait()
+print("run_bgr_flow_part_1.py: all components finished")
+
+for channel in channels:
+    channel.terminate()
+print("run_bgr_flow_part_1.py: all channels terminated")
+
---
+++ run_bgr_flow_slurm.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+# -*- coding: UTF-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+# Authors:
+# Michael Berg-Mohnicke <michael.berg-mohnicke@zalf.de>
+#
+# Maintainers:
+# Currently maintained by the authors.
+#
+# This file has been created at the Institute of
+# Landscape Systems Analysis at the ZALF.
+# Copyright (C: Leibniz Centre for Agricultural Landscape Research (ZALF)
+
+import socket
+import subprocess as sp
+import sys
+import uuid
+
+def get_free_port():
+    with socket.socket() as s:
+        s.bind(('',0))
+        return s.getsockname()[1]
+
+config = {
+    "hpc": False,
+    "use_infiniband": False,
+    "path_to_channel": "/home/berg/GitHub/mas-infrastructure/src/cpp/common/_cmake_linux_release/channel",
+    "path_to_monica": "/home/berg/GitHub/monica/_cmake_linux_release/monica-capnp-fbp-component",
+    "path_to_mas": "/home/berg/GitHub/mas-infrastructure",
+    "path_to_klimertrag": "/home/berg/GitHub/klimertrag",
+    "path_to_out_dir": "/home/berg/GitHub/klimertrag/out_fbp",
+    "setups_file": "/home/berg/GitHub/klimertrag/sim_setups_bgr_flow.csv",
+    "coords_file": "/home/berg/Desktop/all_coord_shuffled_anonymous.csv",
+    "monica_count": "1",
+    "proj_transformer_count": "1",
+    "ilr_count": "1",
+    "dwd_count": "1",
+    "writer_count": "1",
+}
+if len(sys.argv) > 1 and __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        k,v = arg.split("=")
+        if k in config:
+            if v.lower() in ["true", "false"]:
+                config[k] = v.lower() == "true"
+            else:
+                config[k] = v
+
+use_infiniband = config["use_infiniband"]
+node_hostname = socket.gethostname()
+node_fqdn = (node_hostname + ".opa" if use_infiniband else ".service") if config["hpc"] else node_hostname
+node_ip = socket.gethostbyname(node_fqdn)
+
+parts = []
+shared_in_srt = str(uuid.uuid4())
+shared_channel_port = get_free_port()
+_ = sp.Popen([
+    "python", 
+    "{}/run_bgr_flow_part_1.py".format(config["path_to_klimertrag"]), 
+    "hpc={}".format(config["hpc"]),
+    "shared_in_srt={}".format(shared_in_srt),
+    "shared_channel_port={}".format(shared_channel_port),
+    "use_infiniband={}".format(config["use_infiniband"]),
+    "path_to_channel={}".format(config["path_to_channel"]),
+    "path_to_mas={}".format(config["path_to_mas"]),
+    "setups_file={}".format(config["setups_file"]),
+    "coords_file={}".format(config["coords_file"]),
+])
+parts.append(_)
+
+_ = sp.Popen([
+    "python", 
+    "{}/run_bgr_flow_part_2.py".format(config["path_to_klimertrag"]), 
+    "hpc={}".format(config["hpc"]),
+    "shared_in_srt={}".format(shared_in_srt),
+    "shared_channel_port={}".format(shared_channel_port),
+    "use_infiniband={}".format(config["use_infiniband"]),
+    "path_to_channel={}".format(config["path_to_channel"]),
+    "path_to_monica={}".format(config["path_to_monica"]),
+    "path_to_mas={}".format(config["path_to_mas"]),
+    "path_to_klimertrag={}".format(config["path_to_klimertrag"]),
+    "path_to_out_dir={}".format(config["path_to_out_dir"]),
+    "setups_file={}".format(config["setups_file"]),
+    "coords_file={}".format(config["coords_file"]),
+    "monica_count={}".format(config["monica_count"]),
+    "proj_transformer_count={}".format(config["proj_transformer_count"]),
+    "ilr_count={}".format(config["ilr_count"]),
+    "dwd_count={}".format(config["dwd_count"]),
+    "writer_count={}".format(config["writer_count"]),
+])
+parts.append(_)
+
+for part in parts:
+    part.wait()
+print("run_bgr_flow_slurm.py: all parts finished")
+
