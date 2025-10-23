4ad1b2344b79ccc20f20d28056f36ec8cad509c8
palkamarlene <marlene.palka@zalf.de>
2025-10-23 13:00:19 +0200

    Remove comma

--- run-consumer_final.py
+++ run-consumer_final.py
@@ -136,7 +136,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
             "Tra_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
             "TraDef_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
 
-            "Precip_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+            "Precip_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1}
 
         }
         output_keys = list(output_grids.keys())
