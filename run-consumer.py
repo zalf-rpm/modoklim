12763c001b8b26332accae722269cb42a1da9fa4
palkamarlene <marlene.palka@zalf.de>
2026-09-25 09:35:02 +0200

    Reduced outputs

--- run-consumer.py
+++ run-consumer.py
@@ -115,14 +115,14 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         # "Mois": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "Mois_30": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "Mois_90": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
-        # "WaterContent": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "WaterContent": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
 
         # "Act_ET": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "Evaporation": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "Tra": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "TraDef": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
 
-        # "Precip": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Precip": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # # # "Drain": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},            
 
         # # # "Nstress_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
@@ -131,14 +131,14 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         # "PASW_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "PASW_30_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "PASW_90_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
-        # "WaterContent_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "WaterContent_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
 
         # "Act_ET_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "Evaporation_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "Tra_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "TraDef_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
 
-        # "Precip_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Precip_spring": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
 
         "SOC_percent": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         "SOC_total": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
